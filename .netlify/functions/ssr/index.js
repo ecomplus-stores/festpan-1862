dd8d9f6923b72bcfe5bd280e6fb17e1541db2fcd
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2024-07-13 18:55:38 -0300

    chore: update ssr

--- .netlify/functions/ssr/index.js
+++ .netlify/functions/ssr/index.js
@@ -51,5 +51,33 @@ exports.handler = (ev, context, callback) => {
   }
 
   const { ssr } = require('@ecomplus/storefront-renderer/functions/')
-  ssr(req, res)
+  exports.ssr = functions.https.onRequest((req, res) => {
+    const chChar = 'p'
+    if (
+      req.path.length > 1
+      && !req.path.startsWith('/app/')
+      && !req.path.startsWith('/admin/')
+      && !req.path.startsWith('/blog')
+      && !req.path.startsWith('/pages')
+      && !req.path.startsWith('/404')
+    ) {
+      const paths = req.path.split('/').slice(1)
+      if (paths.length < 2) {
+        res
+          .status(301)
+          .set('Location', `/${chChar}${req.path}`)
+          .end()
+        return
+      }
+      const [chsPath] = paths
+      if (!chsPath.includes(chChar)) {
+        res
+          .status(301)
+          .set('Location', req.path.replace(`/${chsPath}/`, `/${chChar}/`))
+          .end()
+        return
+      }
+    }
+    ssr(req, res);
+  });
 }
